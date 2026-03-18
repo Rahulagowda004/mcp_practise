08650c82a999c90d9b574b0709f3bcde3df2ac1c
Rahul Gowda <rahulgowda277@gmail.com>
2026-03-18 12:04:25 +0530

    Add client implementation for MultiServerMCPClient and integrate ChatGroq model

---
+++ client.py
@@ -0,0 +1,40 @@
+import asyncio
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.9)
+
+async def main():
+    client = MultiServerMCPClient(
+        {
+            "math": {
+                "transport": "stdio",
+                "command": "python",
+                "args": ["server.py"],  # path to your server file
+            }
+        }
+    )
+
+    # Load tools from MCP server
+    tools = await client.get_tools()
+
+    # Create agent
+    agent = create_agent(
+        model=llm,
+        tools=tools,
+        system_prompt="Always use tools for calculations. Do not solve on your own."
+    )
+
+    # Run query
+    response = await agent.ainvoke(
+        {"messages": [{"role": "user", "content": "what is (4 + 6) * 5?"}]}
+    )
+
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demo 🚀")
+mcp = FastMCP("Demo Server")
 
-@mcp.tool
+@mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
-@mcp.prompt
-def ask_about_topic(topic: str) -> str:
-    """Generates a user message asking for an explanation of a topic."""
-    return f"Can you please explain the concept of '{topic}'?"
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio", port=8000)
+    mcp.run(transport="stdio")
